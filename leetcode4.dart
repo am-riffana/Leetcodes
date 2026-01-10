64f16d02c11f4f293d8caa0e3c0b050042ed404d
rifana sherin <rifanasherin80@gmail.com>
2026-01-10 09:49:21 +0530

    leetcode4

---
+++ leetcode4.dart
@@ -0,0 +1,23 @@
+class Solution {
+  bool isPalindrome(int x) {
+    if (x < 0) return false; 
+
+  int original = x;
+  int reversed = 0;
+
+  while (x > 0) {
+    int digit = x % 10;           
+    reversed = reversed * 10 + digit; 
+    x ~/= 10;                      
+  }
+
+  return original == reversed;
+}
+
+void main() {
+  print(isPalindrome(121));   
+  
+}
+
+    
+  }
